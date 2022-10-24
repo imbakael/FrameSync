8b540cd57c512770bb9be729d3ac2dc30d14a0e6
JL <woyoureqing@gmail.com>
2022-10-24 10:45:56 +0800

    创建技能配置文件

---
+++ Assets/Scripts/LogicLayer/SkillSystem/Skill/Skill.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SkillState {
+    None,
+    ShakeBefore,
+    ShakeAfter,
+}
+
+public class Skill {
+    
+    public int SkillId { get; private set; }
+    private HeroLogic skillOwner;
+    private bool isNormalAtk;
+    public SkillState SkillState { get; private set; }
+
+    public Skill(int skillId, LogicObject skillOwner, bool isNormalAtk) {
+        SkillId = skillId;
+        skillOwner = (HeroLogic)skillOwner;
+        this.isNormalAtk = isNormalAtk;
+    }
+
+    public void ReleaseSkill() {
+        SkillShakeBefore();
+        PlaySkillAnim();
+        MoveToTarget();
+    }
+
+    public void SkillShakeBefore() {
+        SkillState = SkillState.ShakeBefore;
+    }
+
+    public void PlaySkillAnim() {
+
+    }
+
+    public void MoveToTarget() {
+
+    }
+
+    public void SkillTrigger() {
+
+    }
+
+    public void CreateSkillEffect() {
+
+    }
+
+    public void AddBuff() {
+
+    }
+
+    public void SkillShakeAfter() {
+
+    }
+
+    public void MoveToSeat() {
+
+    }
+
+    public void SkillEnd() {
+
+    }
+}
---
+++ Assets/Scripts/LogicLayer/SkillSystem/Skill/SkillConfig.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkillConfig", menuName = "SkillConfig", order = 0)]
+public class SkillConfig : ScriptableObject {
+    public Sprite skillIcon;
+    public int skillId;
+    public string skillName;
+    public int needRageValue = 100;
+    public int skillShakeBeforeTimeMs;
+    public int skillAttackDurationMs;
+    public int skillShakeAfterTimeMs;
+    public SkillType skillType;
+    public TargetType targetType;
+    public SkillAttackType skillAttackType;
+    public DamageType damageType;
+    public int damagePercentage;
+
+    // 渲染层数据
+    public string bullet;
+    public string skillAnim;
+    public AudioClip skillAudio;
+    public string skillEffect;
+    public string skillHitEffect;
+    public int[] addBuffs;
+    public string skillDes;
+}
+
+public enum SkillType {
+    MoveToAttack,
+    MoveToEnemyCenter,
+    MoveToCenter,
+    Chant,
+   Ballistic
+}
+
+public enum TargetType {
+    None,
+    Teammate,
+    Enemy
+}
+
+public enum SkillAttackType {
+    SingleTarget,
+    AllHero,
+    BackRowHero,
+    FrontRowHero,
+    SamColumnHero
+}
+
+public enum DamageType {
+    None,
+    NormalDamage,
+    RealDamage,
+    AtkPercentage,
+    HpPercentage
+}
